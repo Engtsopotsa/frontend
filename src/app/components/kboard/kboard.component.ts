598707af3d29e7eace8ba60dc7b932176bb30f62
Tapiwa Tsopotsa <tapiwatsopotsa1@gmail.com>
2023-03-25 16:57:22 +0200

    added the kboard component

---
+++ src/app/components/kboard/kboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kboard',
+  templateUrl: './kboard.component.html',
+  styleUrls: ['./kboard.component.css']
+})
+export class KboardComponent {
+
+}
